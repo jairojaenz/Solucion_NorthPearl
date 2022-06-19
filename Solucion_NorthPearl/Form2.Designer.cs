0e0034dc2e31b3941fc3199547f3f8655604e51f
jairo jaenz <jairojaenz91@gmail.com>
2022-06-19 17:44:56 -0600

    insertar logo en segunda forma

--- Solucion_NorthPearl/Form2.Designer.cs
+++ Solucion_NorthPearl/Form2.Designer.cs
@@ -42,13 +42,15 @@
             this.cbbxEntrada = new System.Windows.Forms.ComboBox();
             this.txtTelefono = new System.Windows.Forms.TextBox();
             this.btnCrearFrm2 = new System.Windows.Forms.Button();
+            this.picbxLogofrm2 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.picbxLogofrm2)).BeginInit();
             this.SuspendLayout();
             // 
             // lblCrearCuenta
             // 
             this.lblCrearCuenta.AutoSize = true;
             this.lblCrearCuenta.Font = new System.Drawing.Font("Palatino Linotype", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.lblCrearCuenta.Location = new System.Drawing.Point(88, 26);
+            this.lblCrearCuenta.Location = new System.Drawing.Point(88, 55);
             this.lblCrearCuenta.Name = "lblCrearCuenta";
             this.lblCrearCuenta.Size = new System.Drawing.Size(132, 26);
             this.lblCrearCuenta.TabIndex = 0;
@@ -58,7 +60,7 @@
             // 
             this.lblNombre.AutoSize = true;
             this.lblNombre.Font = new System.Drawing.Font("Palatino Linotype", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.lblNombre.Location = new System.Drawing.Point(75, 78);
+            this.lblNombre.Location = new System.Drawing.Point(76, 117);
             this.lblNombre.Name = "lblNombre";
             this.lblNombre.Size = new System.Drawing.Size(58, 18);
             this.lblNombre.TabIndex = 1;
@@ -66,14 +68,14 @@
             // 
             // txtNombre
             // 
-            this.txtNombre.Location = new System.Drawing.Point(75, 105);
+            this.txtNombre.Location = new System.Drawing.Point(76, 144);
             this.txtNombre.Name = "txtNombre";
             this.txtNombre.Size = new System.Drawing.Size(184, 23);
             this.txtNombre.TabIndex = 2;
             // 
             // txtCorreo
             // 
-            this.txtCorreo.Location = new System.Drawing.Point(75, 181);
+            this.txtCorreo.Location = new System.Drawing.Point(76, 220);
             this.txtCorreo.Name = "txtCorreo";
             this.txtCorreo.Size = new System.Drawing.Size(184, 23);
             this.txtCorreo.TabIndex = 4;
@@ -82,7 +84,7 @@
             // 
             this.lblCorreo.AutoSize = true;
             this.lblCorreo.Font = new System.Drawing.Font("Palatino Linotype", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.lblCorreo.Location = new System.Drawing.Point(75, 154);
+            this.lblCorreo.Location = new System.Drawing.Point(76, 193);
             this.lblCorreo.Name = "lblCorreo";
             this.lblCorreo.Size = new System.Drawing.Size(49, 18);
             this.lblCorreo.TabIndex = 3;
@@ -90,7 +92,7 @@
             // 
             // txtContrasena
             // 
-            this.txtContrasena.Location = new System.Drawing.Point(75, 256);
+            this.txtContrasena.Location = new System.Drawing.Point(76, 295);
             this.txtContrasena.Name = "txtContrasena";
             this.txtContrasena.PasswordChar = '*';
             this.txtContrasena.Size = new System.Drawing.Size(184, 23);
@@ -100,7 +102,7 @@
             // 
             this.lblContrasena.AutoSize = true;
             this.lblContrasena.Font = new System.Drawing.Font("Palatino Linotype", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.lblContrasena.Location = new System.Drawing.Point(75, 229);
+            this.lblContrasena.Location = new System.Drawing.Point(76, 268);
             this.lblContrasena.Name = "lblContrasena";
             this.lblContrasena.Size = new System.Drawing.Size(77, 18);
             this.lblContrasena.TabIndex = 5;
@@ -110,7 +112,7 @@
             // 
             this.lblNacionalidad.AutoSize = true;
             this.lblNacionalidad.Font = new System.Drawing.Font("Palatino Linotype", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.lblNacionalidad.Location = new System.Drawing.Point(75, 292);
+            this.lblNacionalidad.Location = new System.Drawing.Point(76, 331);
             this.lblNacionalidad.Name = "lblNacionalidad";
             this.lblNacionalidad.Size = new System.Drawing.Size(89, 18);
             this.lblNacionalidad.TabIndex = 7;
@@ -119,7 +121,7 @@
             // cbbxNacionalidad
             // 
             this.cbbxNacionalidad.FormattingEnabled = true;
-            this.cbbxNacionalidad.Location = new System.Drawing.Point(75, 321);
+            this.cbbxNacionalidad.Location = new System.Drawing.Point(76, 360);
             this.cbbxNacionalidad.Name = "cbbxNacionalidad";
             this.cbbxNacionalidad.Size = new System.Drawing.Size(184, 23);
             this.cbbxNacionalidad.TabIndex = 8;
@@ -128,7 +130,7 @@
             // 
             this.lblTelefono.AutoSize = true;
             this.lblTelefono.Font = new System.Drawing.Font("Palatino Linotype", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.lblTelefono.Location = new System.Drawing.Point(75, 368);
+            this.lblTelefono.Location = new System.Drawing.Point(76, 407);
             this.lblTelefono.Name = "lblTelefono";
             this.lblTelefono.Size = new System.Drawing.Size(57, 18);
             this.lblTelefono.TabIndex = 9;
@@ -137,7 +139,7 @@
             // cbbxEntrada
             // 
             this.cbbxEntrada.FormattingEnabled = true;
-            this.cbbxEntrada.Location = new System.Drawing.Point(75, 389);
+            this.cbbxEntrada.Location = new System.Drawing.Point(76, 428);
             this.cbbxEntrada.Name = "cbbxEntrada";
             this.cbbxEntrada.Size = new System.Drawing.Size(41, 23);
             this.cbbxEntrada.TabIndex = 10;
@@ -145,7 +147,7 @@
             // 
             // txtTelefono
             // 
-            this.txtTelefono.Location = new System.Drawing.Point(122, 389);
+            this.txtTelefono.Location = new System.Drawing.Point(123, 428);
             this.txtTelefono.Name = "txtTelefono";
             this.txtTelefono.Size = new System.Drawing.Size(137, 23);
             this.txtTelefono.TabIndex = 11;
@@ -158,7 +160,7 @@
             this.btnCrearFrm2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.btnCrearFrm2.Font = new System.Drawing.Font("Palatino Linotype", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
             this.btnCrearFrm2.ForeColor = System.Drawing.Color.White;
-            this.btnCrearFrm2.Location = new System.Drawing.Point(113, 446);
+            this.btnCrearFrm2.Location = new System.Drawing.Point(114, 485);
             this.btnCrearFrm2.Name = "btnCrearFrm2";
             this.btnCrearFrm2.Size = new System.Drawing.Size(97, 23);
             this.btnCrearFrm2.TabIndex = 12;
@@ -166,12 +168,23 @@
             this.btnCrearFrm2.UseVisualStyleBackColor = false;
             this.btnCrearFrm2.Click += new System.EventHandler(this.btnCrearFrm2_Click);
             // 
+            // picbxLogofrm2
+            // 
+            this.picbxLogofrm2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("picbxLogofrm2.BackgroundImage")));
+            this.picbxLogofrm2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.picbxLogofrm2.Location = new System.Drawing.Point(226, 9);
+            this.picbxLogofrm2.Name = "picbxLogofrm2";
+            this.picbxLogofrm2.Size = new System.Drawing.Size(100, 50);
+            this.picbxLogofrm2.TabIndex = 13;
+            this.picbxLogofrm2.TabStop = false;
+            // 
             // frmCrearCuenta
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
             this.ClientSize = new System.Drawing.Size(312, 531);
+            this.Controls.Add(this.picbxLogofrm2);
             this.Controls.Add(this.btnCrearFrm2);
             this.Controls.Add(this.txtTelefono);
             this.Controls.Add(this.cbbxEntrada);
@@ -188,6 +201,7 @@
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "frmCrearCuenta";
             this.Text = "Crear Cuenta";
+            ((System.ComponentModel.ISupportInitialize)(this.picbxLogofrm2)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -208,5 +222,6 @@
         private ComboBox cbbxEntrada;
         private TextBox txtTelefono;
         private Button btnCrearFrm2;
+        private PictureBox picbxLogofrm2;
     }
 }
