a4026906d94604f886bb1d7aa766f1b7d5ef3827
jairo jaenz <jairojaenz91@gmail.com>
2022-06-19 17:31:51 -0600

    Diseño de la segunda forma (crear cuenta)

--- Solucion_NorthPearl/Form2.Designer.cs
+++ Solucion_NorthPearl/Form2.Designer.cs
@@ -29,20 +29,184 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmCrearCuenta));
+            this.lblCrearCuenta = new System.Windows.Forms.Label();
+            this.lblNombre = new System.Windows.Forms.Label();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.txtCorreo = new System.Windows.Forms.TextBox();
+            this.lblCorreo = new System.Windows.Forms.Label();
+            this.txtContrasena = new System.Windows.Forms.TextBox();
+            this.lblContrasena = new System.Windows.Forms.Label();
+            this.lblNacionalidad = new System.Windows.Forms.Label();
+            this.cbbxNacionalidad = new System.Windows.Forms.ComboBox();
+            this.lblTelefono = new System.Windows.Forms.Label();
+            this.cbbxEntrada = new System.Windows.Forms.ComboBox();
+            this.txtTelefono = new System.Windows.Forms.TextBox();
+            this.btnCrearFrm2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // lblCrearCuenta
+            // 
+            this.lblCrearCuenta.AutoSize = true;
+            this.lblCrearCuenta.Font = new System.Drawing.Font("Palatino Linotype", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblCrearCuenta.Location = new System.Drawing.Point(88, 26);
+            this.lblCrearCuenta.Name = "lblCrearCuenta";
+            this.lblCrearCuenta.Size = new System.Drawing.Size(132, 26);
+            this.lblCrearCuenta.TabIndex = 0;
+            this.lblCrearCuenta.Text = "Crear Cuenta";
+            // 
+            // lblNombre
+            // 
+            this.lblNombre.AutoSize = true;
+            this.lblNombre.Font = new System.Drawing.Font("Palatino Linotype", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblNombre.Location = new System.Drawing.Point(75, 78);
+            this.lblNombre.Name = "lblNombre";
+            this.lblNombre.Size = new System.Drawing.Size(58, 18);
+            this.lblNombre.TabIndex = 1;
+            this.lblNombre.Text = "Nombre";
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(75, 105);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(184, 23);
+            this.txtNombre.TabIndex = 2;
+            // 
+            // txtCorreo
+            // 
+            this.txtCorreo.Location = new System.Drawing.Point(75, 181);
+            this.txtCorreo.Name = "txtCorreo";
+            this.txtCorreo.Size = new System.Drawing.Size(184, 23);
+            this.txtCorreo.TabIndex = 4;
+            // 
+            // lblCorreo
+            // 
+            this.lblCorreo.AutoSize = true;
+            this.lblCorreo.Font = new System.Drawing.Font("Palatino Linotype", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblCorreo.Location = new System.Drawing.Point(75, 154);
+            this.lblCorreo.Name = "lblCorreo";
+            this.lblCorreo.Size = new System.Drawing.Size(49, 18);
+            this.lblCorreo.TabIndex = 3;
+            this.lblCorreo.Text = "Correo";
+            // 
+            // txtContrasena
+            // 
+            this.txtContrasena.Location = new System.Drawing.Point(75, 256);
+            this.txtContrasena.Name = "txtContrasena";
+            this.txtContrasena.PasswordChar = '*';
+            this.txtContrasena.Size = new System.Drawing.Size(184, 23);
+            this.txtContrasena.TabIndex = 6;
+            // 
+            // lblContrasena
+            // 
+            this.lblContrasena.AutoSize = true;
+            this.lblContrasena.Font = new System.Drawing.Font("Palatino Linotype", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblContrasena.Location = new System.Drawing.Point(75, 229);
+            this.lblContrasena.Name = "lblContrasena";
+            this.lblContrasena.Size = new System.Drawing.Size(77, 18);
+            this.lblContrasena.TabIndex = 5;
+            this.lblContrasena.Text = "Contraseña";
+            // 
+            // lblNacionalidad
+            // 
+            this.lblNacionalidad.AutoSize = true;
+            this.lblNacionalidad.Font = new System.Drawing.Font("Palatino Linotype", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblNacionalidad.Location = new System.Drawing.Point(75, 292);
+            this.lblNacionalidad.Name = "lblNacionalidad";
+            this.lblNacionalidad.Size = new System.Drawing.Size(89, 18);
+            this.lblNacionalidad.TabIndex = 7;
+            this.lblNacionalidad.Text = "Nacionalidad";
+            // 
+            // cbbxNacionalidad
+            // 
+            this.cbbxNacionalidad.FormattingEnabled = true;
+            this.cbbxNacionalidad.Location = new System.Drawing.Point(75, 321);
+            this.cbbxNacionalidad.Name = "cbbxNacionalidad";
+            this.cbbxNacionalidad.Size = new System.Drawing.Size(184, 23);
+            this.cbbxNacionalidad.TabIndex = 8;
+            // 
+            // lblTelefono
+            // 
+            this.lblTelefono.AutoSize = true;
+            this.lblTelefono.Font = new System.Drawing.Font("Palatino Linotype", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblTelefono.Location = new System.Drawing.Point(75, 368);
+            this.lblTelefono.Name = "lblTelefono";
+            this.lblTelefono.Size = new System.Drawing.Size(57, 18);
+            this.lblTelefono.TabIndex = 9;
+            this.lblTelefono.Text = "Telefono";
+            // 
+            // cbbxEntrada
+            // 
+            this.cbbxEntrada.FormattingEnabled = true;
+            this.cbbxEntrada.Location = new System.Drawing.Point(75, 389);
+            this.cbbxEntrada.Name = "cbbxEntrada";
+            this.cbbxEntrada.Size = new System.Drawing.Size(41, 23);
+            this.cbbxEntrada.TabIndex = 10;
+            this.cbbxEntrada.Text = "+1";
+            // 
+            // txtTelefono
+            // 
+            this.txtTelefono.Location = new System.Drawing.Point(122, 389);
+            this.txtTelefono.Name = "txtTelefono";
+            this.txtTelefono.Size = new System.Drawing.Size(137, 23);
+            this.txtTelefono.TabIndex = 11;
+            // 
+            // btnCrearFrm2
+            // 
+            this.btnCrearFrm2.BackColor = System.Drawing.Color.Blue;
+            this.btnCrearFrm2.FlatAppearance.BorderColor = System.Drawing.Color.Blue;
+            this.btnCrearFrm2.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Cyan;
+            this.btnCrearFrm2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnCrearFrm2.Font = new System.Drawing.Font("Palatino Linotype", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnCrearFrm2.ForeColor = System.Drawing.Color.White;
+            this.btnCrearFrm2.Location = new System.Drawing.Point(113, 446);
+            this.btnCrearFrm2.Name = "btnCrearFrm2";
+            this.btnCrearFrm2.Size = new System.Drawing.Size(97, 23);
+            this.btnCrearFrm2.TabIndex = 12;
+            this.btnCrearFrm2.Text = "Crear";
+            this.btnCrearFrm2.UseVisualStyleBackColor = false;
+            this.btnCrearFrm2.Click += new System.EventHandler(this.btnCrearFrm2_Click);
             // 
             // frmCrearCuenta
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(312, 450);
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(312, 531);
+            this.Controls.Add(this.btnCrearFrm2);
+            this.Controls.Add(this.txtTelefono);
+            this.Controls.Add(this.cbbxEntrada);
+            this.Controls.Add(this.lblTelefono);
+            this.Controls.Add(this.cbbxNacionalidad);
+            this.Controls.Add(this.lblNacionalidad);
+            this.Controls.Add(this.txtContrasena);
+            this.Controls.Add(this.lblContrasena);
+            this.Controls.Add(this.txtCorreo);
+            this.Controls.Add(this.lblCorreo);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.lblNombre);
+            this.Controls.Add(this.lblCrearCuenta);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "frmCrearCuenta";
             this.Text = "Crear Cuenta";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private Label lblCrearCuenta;
+        private Label lblNombre;
+        private TextBox txtNombre;
+        private TextBox txtCorreo;
+        private Label lblCorreo;
+        private TextBox txtContrasena;
+        private Label lblContrasena;
+        private Label lblNacionalidad;
+        private ComboBox cbbxNacionalidad;
+        private Label lblTelefono;
+        private ComboBox cbbxEntrada;
+        private TextBox txtTelefono;
+        private Button btnCrearFrm2;
     }
 }
--- Solucion_NorthPearl/Form2.cs
+++ Solucion_NorthPearl/Form2.cs
@@ -22,5 +22,10 @@ namespace Solucion_NorthPearl
             this.Close();
                 
         }
+
+        private void btnCrearFrm2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
     }
 }
