734379ea48cd8c1be0c3d5a4a60d934eedd63454
jairo jaenz <jairojaenz91@gmail.com>
2022-06-19 16:26:15 -0600

    Agregar botones y linklabel

--- Solucion_NorthPearl/Form1.Designer.cs
+++ Solucion_NorthPearl/Form1.Designer.cs
@@ -36,8 +36,18 @@
             this.txtCorreo = new System.Windows.Forms.TextBox();
             this.txtContrasena = new System.Windows.Forms.TextBox();
             this.llbContrasena = new System.Windows.Forms.Label();
+            this.lblRecupContra = new System.Windows.Forms.Label();
+            this.linklblRecuperar = new System.Windows.Forms.LinkLabel();
+            this.btnCrearCuenta = new System.Windows.Forms.Button();
+            this.btnIniciarSesion = new System.Windows.Forms.Button();
+            this.lblContinuarCon = new System.Windows.Forms.Label();
+            this.lblÓ = new System.Windows.Forms.Label();
+            this.picbxFacebook = new System.Windows.Forms.PictureBox();
+            this.picbxGoogle = new System.Windows.Forms.PictureBox();
             ((System.ComponentModel.ISupportInitialize)(this.picbxUsuario)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.picbxLogoApp)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picbxFacebook)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picbxGoogle)).BeginInit();
             this.SuspendLayout();
             // 
             // llbIniciarSesion
@@ -52,6 +62,8 @@
             // 
             // picbxUsuario
             // 
+            this.picbxUsuario.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("picbxUsuario.BackgroundImage")));
+            this.picbxUsuario.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
             this.picbxUsuario.Location = new System.Drawing.Point(30, 57);
             this.picbxUsuario.Name = "picbxUsuario";
             this.picbxUsuario.Size = new System.Drawing.Size(100, 50);
@@ -60,6 +72,8 @@
             // 
             // picbxLogoApp
             // 
+            this.picbxLogoApp.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("picbxLogoApp.BackgroundImage")));
+            this.picbxLogoApp.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.picbxLogoApp.Location = new System.Drawing.Point(172, 57);
             this.picbxLogoApp.Name = "picbxLogoApp";
             this.picbxLogoApp.Size = new System.Drawing.Size(100, 50);
@@ -85,7 +99,7 @@
             // 
             // txtContrasena
             // 
-            this.txtContrasena.Location = new System.Drawing.Point(30, 236);
+            this.txtContrasena.Location = new System.Drawing.Point(30, 225);
             this.txtContrasena.Name = "txtContrasena";
             this.txtContrasena.PasswordChar = '*';
             this.txtContrasena.Size = new System.Drawing.Size(233, 23);
@@ -95,18 +109,117 @@
             // 
             this.llbContrasena.AutoSize = true;
             this.llbContrasena.Font = new System.Drawing.Font("Palatino Linotype", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.llbContrasena.Location = new System.Drawing.Point(30, 218);
+            this.llbContrasena.Location = new System.Drawing.Point(30, 207);
             this.llbContrasena.Name = "llbContrasena";
             this.llbContrasena.Size = new System.Drawing.Size(77, 18);
             this.llbContrasena.TabIndex = 5;
             this.llbContrasena.Text = "Contraseña";
+            // 
+            // lblRecupContra
+            // 
+            this.lblRecupContra.AutoSize = true;
+            this.lblRecupContra.Font = new System.Drawing.Font("Palatino Linotype", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblRecupContra.Location = new System.Drawing.Point(88, 268);
+            this.lblRecupContra.Name = "lblRecupContra";
+            this.lblRecupContra.Size = new System.Drawing.Size(155, 18);
+            this.lblRecupContra.TabIndex = 7;
+            this.lblRecupContra.Text = "Olvidaste la contraseña?";
+            // 
+            // linklblRecuperar
+            // 
+            this.linklblRecuperar.AutoSize = true;
+            this.linklblRecuperar.Font = new System.Drawing.Font("Palatino Linotype", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.linklblRecuperar.Location = new System.Drawing.Point(240, 268);
+            this.linklblRecuperar.Name = "linklblRecuperar";
+            this.linklblRecuperar.Size = new System.Drawing.Size(70, 18);
+            this.linklblRecuperar.TabIndex = 8;
+            this.linklblRecuperar.TabStop = true;
+            this.linklblRecuperar.Text = "Recuperar";
+            // 
+            // btnCrearCuenta
+            // 
+            this.btnCrearCuenta.BackColor = System.Drawing.Color.Blue;
+            this.btnCrearCuenta.FlatAppearance.BorderColor = System.Drawing.Color.Blue;
+            this.btnCrearCuenta.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Cyan;
+            this.btnCrearCuenta.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnCrearCuenta.Font = new System.Drawing.Font("Palatino Linotype", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnCrearCuenta.ForeColor = System.Drawing.Color.White;
+            this.btnCrearCuenta.Location = new System.Drawing.Point(28, 307);
+            this.btnCrearCuenta.Name = "btnCrearCuenta";
+            this.btnCrearCuenta.Size = new System.Drawing.Size(97, 23);
+            this.btnCrearCuenta.TabIndex = 9;
+            this.btnCrearCuenta.Text = "Crear Cuenta";
+            this.btnCrearCuenta.UseVisualStyleBackColor = false;
+            // 
+            // btnIniciarSesion
+            // 
+            this.btnIniciarSesion.BackColor = System.Drawing.Color.Blue;
+            this.btnIniciarSesion.FlatAppearance.BorderColor = System.Drawing.Color.Blue;
+            this.btnIniciarSesion.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Cyan;
+            this.btnIniciarSesion.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnIniciarSesion.Font = new System.Drawing.Font("Palatino Linotype", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnIniciarSesion.ForeColor = System.Drawing.Color.White;
+            this.btnIniciarSesion.Location = new System.Drawing.Point(166, 307);
+            this.btnIniciarSesion.Name = "btnIniciarSesion";
+            this.btnIniciarSesion.Size = new System.Drawing.Size(106, 23);
+            this.btnIniciarSesion.TabIndex = 10;
+            this.btnIniciarSesion.Text = "Iniciar Sesión";
+            this.btnIniciarSesion.UseVisualStyleBackColor = false;
+            // 
+            // lblContinuarCon
+            // 
+            this.lblContinuarCon.AutoSize = true;
+            this.lblContinuarCon.Font = new System.Drawing.Font("Palatino Linotype", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblContinuarCon.Location = new System.Drawing.Point(107, 343);
+            this.lblContinuarCon.Name = "lblContinuarCon";
+            this.lblContinuarCon.Size = new System.Drawing.Size(103, 18);
+            this.lblContinuarCon.TabIndex = 11;
+            this.lblContinuarCon.Text = "Continuar Con :";
+            // 
+            // lblÓ
+            // 
+            this.lblÓ.AutoSize = true;
+            this.lblÓ.Font = new System.Drawing.Font("Palatino Linotype", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblÓ.Location = new System.Drawing.Point(145, 397);
+            this.lblÓ.Name = "lblÓ";
+            this.lblÓ.Size = new System.Drawing.Size(18, 18);
+            this.lblÓ.TabIndex = 13;
+            this.lblÓ.Text = "Ó";
+            // 
+            // picbxFacebook
+            // 
+            this.picbxFacebook.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("picbxFacebook.BackgroundImage")));
+            this.picbxFacebook.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.picbxFacebook.Location = new System.Drawing.Point(136, 364);
+            this.picbxFacebook.Name = "picbxFacebook";
+            this.picbxFacebook.Size = new System.Drawing.Size(35, 30);
+            this.picbxFacebook.TabIndex = 15;
+            this.picbxFacebook.TabStop = false;
+            // 
+            // picbxGoogle
+            // 
+            this.picbxGoogle.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("picbxGoogle.BackgroundImage")));
+            this.picbxGoogle.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.picbxGoogle.Location = new System.Drawing.Point(136, 418);
+            this.picbxGoogle.Name = "picbxGoogle";
+            this.picbxGoogle.Size = new System.Drawing.Size(35, 30);
+            this.picbxGoogle.TabIndex = 16;
+            this.picbxGoogle.TabStop = false;
             // 
             // frmNorthPearl
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(308, 402);
+            this.ClientSize = new System.Drawing.Size(308, 455);
+            this.Controls.Add(this.picbxGoogle);
+            this.Controls.Add(this.picbxFacebook);
+            this.Controls.Add(this.lblÓ);
+            this.Controls.Add(this.lblContinuarCon);
+            this.Controls.Add(this.btnIniciarSesion);
+            this.Controls.Add(this.btnCrearCuenta);
+            this.Controls.Add(this.linklblRecuperar);
+            this.Controls.Add(this.lblRecupContra);
             this.Controls.Add(this.txtContrasena);
             this.Controls.Add(this.llbContrasena);
             this.Controls.Add(this.txtCorreo);
@@ -119,6 +232,8 @@
             this.Text = "Noth Pearl";
             ((System.ComponentModel.ISupportInitialize)(this.picbxUsuario)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.picbxLogoApp)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picbxFacebook)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picbxGoogle)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -133,5 +248,13 @@
         private TextBox txtCorreo;
         private TextBox txtContrasena;
         private Label llbContrasena;
+        private Label lblRecupContra;
+        private LinkLabel linklblRecuperar;
+        private Button btnCrearCuenta;
+        private Button btnIniciarSesion;
+        private Label lblContinuarCon;
+        private Label lblÓ;
+        private PictureBox picbxFacebook;
+        private PictureBox picbxGoogle;
     }
 }
