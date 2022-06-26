f80efc0bfb851f23d9f2212717e06b4ce781e71d
jairo jaenz <jairojaenz91@gmail.com>
2022-06-26 17:12:58 -0600

    codigo de validar datos con un solo usuario

--- Solucion_NorthPearl/Form1.cs
+++ Solucion_NorthPearl/Form1.cs
@@ -1,7 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.IO;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
 namespace Solucion_NorthPearl
 {
     public partial class frmNorthPearl : Form
     {
+       static bool encontrado;
+        static StreamReader lectura;
+        static string cadena;
+        static string[] registro = new string[4];
+        
         public frmNorthPearl()
         {
             InitializeComponent();
@@ -21,9 +36,31 @@ namespace Solucion_NorthPearl
 
         private void btnIniciarSesion_Click(object sender, EventArgs e)
         {
-            frmPantallaPrincipal miforma3= new frmPantallaPrincipal();
-            miforma3.Show();
-            this.Hide();
+            encontrado = false;
+            if (txtCorreo.Text != "" && txtContrasena.Text != "")
+            {
+                lectura = File.OpenText("usuarios.txt");
+                cadena = lectura.ReadLine();               
+                    registro = cadena.Split(',');
+                if (registro[0].Trim().Equals(txtCorreo.Text) && registro[1].Trim().Equals(txtContrasena.Text))
+                {
+                    MessageBox.Show("usuario encontrado", "iniciar sesion", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    encontrado = true;
+                    this.Hide();
+                    frmPantallaPrincipal forma5 = new frmPantallaPrincipal();
+                    forma5.ShowDialog();
+
+                    cadena = lectura.ReadLine();
+                }
+                else
+                {
+                    MessageBox.Show("usuario no encontrado", "aviso", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Faltan Datos", "aviso", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
         }
     }
 }
