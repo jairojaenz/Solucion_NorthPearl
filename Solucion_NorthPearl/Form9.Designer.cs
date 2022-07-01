15f95e1053f6337fd7a4b53fdd06a93cdc07caa2
FER450777 <fernando1320777@gmail.com>
2022-07-01 16:00:20 -0600

    Creación de un nueva forma para registrar sitios

---
+++ Solucion_NorthPearl/Form9.Designer.cs
@@ -0,0 +1,274 @@
+﻿namespace Solucion_NorthPearl
+{
+    partial class FrmRegistroSitios
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmRegistroSitios));
+            this.picbxLogofrm2 = new System.Windows.Forms.PictureBox();
+            this.btnGuardar = new System.Windows.Forms.Button();
+            this.txtNombreDueno = new System.Windows.Forms.TextBox();
+            this.lblNombreDueno = new System.Windows.Forms.Label();
+            this.txtUbicacionSitio = new System.Windows.Forms.TextBox();
+            this.lblUbicacionSitio = new System.Windows.Forms.Label();
+            this.txtNombreSitio = new System.Windows.Forms.TextBox();
+            this.lblNombreSitio = new System.Windows.Forms.Label();
+            this.lblRegistroSitio = new System.Windows.Forms.Label();
+            this.txtNumeroTelefono = new System.Windows.Forms.TextBox();
+            this.lblNumeroTelefono = new System.Windows.Forms.Label();
+            this.txtCorreoSitio = new System.Windows.Forms.TextBox();
+            this.lblCorreoSitio = new System.Windows.Forms.Label();
+            this.txtHorarioAtencion = new System.Windows.Forms.TextBox();
+            this.lblHorarioAtencion = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.lblCostoServicio = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.picbxLogofrm2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // picbxLogofrm2
+            // 
+            this.picbxLogofrm2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("picbxLogofrm2.BackgroundImage")));
+            this.picbxLogofrm2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.picbxLogofrm2.Location = new System.Drawing.Point(209, 2);
+            this.picbxLogofrm2.Name = "picbxLogofrm2";
+            this.picbxLogofrm2.Size = new System.Drawing.Size(100, 50);
+            this.picbxLogofrm2.TabIndex = 27;
+            this.picbxLogofrm2.TabStop = false;
+            // 
+            // btnGuardar
+            // 
+            this.btnGuardar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.btnGuardar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnGuardar.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.btnGuardar.FlatAppearance.BorderSize = 2;
+            this.btnGuardar.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Green;
+            this.btnGuardar.FlatAppearance.MouseOverBackColor = System.Drawing.Color.LimeGreen;
+            this.btnGuardar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnGuardar.Font = new System.Drawing.Font("Palatino Linotype", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnGuardar.ForeColor = System.Drawing.Color.White;
+            this.btnGuardar.Location = new System.Drawing.Point(194, 500);
+            this.btnGuardar.Name = "btnGuardar";
+            this.btnGuardar.Size = new System.Drawing.Size(97, 28);
+            this.btnGuardar.TabIndex = 26;
+            this.btnGuardar.Text = "Guardar";
+            this.btnGuardar.UseVisualStyleBackColor = false;
+            // 
+            // txtNombreDueno
+            // 
+            this.txtNombreDueno.Location = new System.Drawing.Point(12, 223);
+            this.txtNombreDueno.Name = "txtNombreDueno";
+            this.txtNombreDueno.PasswordChar = '*';
+            this.txtNombreDueno.Size = new System.Drawing.Size(279, 23);
+            this.txtNombreDueno.TabIndex = 21;
+            // 
+            // lblNombreDueno
+            // 
+            this.lblNombreDueno.AutoSize = true;
+            this.lblNombreDueno.Font = new System.Drawing.Font("Palatino Linotype", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblNombreDueno.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.lblNombreDueno.Location = new System.Drawing.Point(15, 199);
+            this.lblNombreDueno.Name = "lblNombreDueno";
+            this.lblNombreDueno.Size = new System.Drawing.Size(229, 36);
+            this.lblNombreDueno.TabIndex = 20;
+            this.lblNombreDueno.Text = "Escriba el nombre del dueño del sitio:\r\n\r\n";
+            // 
+            // txtUbicacionSitio
+            // 
+            this.txtUbicacionSitio.Location = new System.Drawing.Point(12, 171);
+            this.txtUbicacionSitio.Name = "txtUbicacionSitio";
+            this.txtUbicacionSitio.Size = new System.Drawing.Size(279, 23);
+            this.txtUbicacionSitio.TabIndex = 19;
+            // 
+            // lblUbicacionSitio
+            // 
+            this.lblUbicacionSitio.AutoSize = true;
+            this.lblUbicacionSitio.Font = new System.Drawing.Font("Palatino Linotype", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblUbicacionSitio.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.lblUbicacionSitio.Location = new System.Drawing.Point(16, 144);
+            this.lblUbicacionSitio.Name = "lblUbicacionSitio";
+            this.lblUbicacionSitio.Size = new System.Drawing.Size(180, 18);
+            this.lblUbicacionSitio.TabIndex = 18;
+            this.lblUbicacionSitio.Text = "Escriba la ubicación del sitio:";
+            // 
+            // txtNombreSitio
+            // 
+            this.txtNombreSitio.Location = new System.Drawing.Point(12, 104);
+            this.txtNombreSitio.Name = "txtNombreSitio";
+            this.txtNombreSitio.Size = new System.Drawing.Size(279, 23);
+            this.txtNombreSitio.TabIndex = 17;
+            // 
+            // lblNombreSitio
+            // 
+            this.lblNombreSitio.AutoSize = true;
+            this.lblNombreSitio.Font = new System.Drawing.Font("Palatino Linotype", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblNombreSitio.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.lblNombreSitio.Location = new System.Drawing.Point(17, 83);
+            this.lblNombreSitio.Name = "lblNombreSitio";
+            this.lblNombreSitio.Size = new System.Drawing.Size(155, 18);
+            this.lblNombreSitio.TabIndex = 16;
+            this.lblNombreSitio.Text = "Escriba nombre del sitio:";
+            // 
+            // lblRegistroSitio
+            // 
+            this.lblRegistroSitio.AutoSize = true;
+            this.lblRegistroSitio.Font = new System.Drawing.Font("Palatino Linotype", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblRegistroSitio.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.lblRegistroSitio.Location = new System.Drawing.Point(57, 48);
+            this.lblRegistroSitio.Name = "lblRegistroSitio";
+            this.lblRegistroSitio.Size = new System.Drawing.Size(195, 26);
+            this.lblRegistroSitio.TabIndex = 15;
+            this.lblRegistroSitio.Text = "Registro de los sitios";
+            // 
+            // txtNumeroTelefono
+            // 
+            this.txtNumeroTelefono.Location = new System.Drawing.Point(12, 283);
+            this.txtNumeroTelefono.Name = "txtNumeroTelefono";
+            this.txtNumeroTelefono.PasswordChar = '*';
+            this.txtNumeroTelefono.Size = new System.Drawing.Size(279, 23);
+            this.txtNumeroTelefono.TabIndex = 29;
+            // 
+            // lblNumeroTelefono
+            // 
+            this.lblNumeroTelefono.AutoSize = true;
+            this.lblNumeroTelefono.Font = new System.Drawing.Font("Palatino Linotype", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblNumeroTelefono.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.lblNumeroTelefono.Location = new System.Drawing.Point(17, 255);
+            this.lblNumeroTelefono.Name = "lblNumeroTelefono";
+            this.lblNumeroTelefono.Size = new System.Drawing.Size(234, 18);
+            this.lblNumeroTelefono.TabIndex = 28;
+            this.lblNumeroTelefono.Text = "Ingrese el número de teléfono del sitio:";
+            // 
+            // txtCorreoSitio
+            // 
+            this.txtCorreoSitio.Location = new System.Drawing.Point(12, 341);
+            this.txtCorreoSitio.Name = "txtCorreoSitio";
+            this.txtCorreoSitio.PasswordChar = '*';
+            this.txtCorreoSitio.Size = new System.Drawing.Size(279, 23);
+            this.txtCorreoSitio.TabIndex = 31;
+            this.txtCorreoSitio.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // lblCorreoSitio
+            // 
+            this.lblCorreoSitio.AutoSize = true;
+            this.lblCorreoSitio.Font = new System.Drawing.Font("Palatino Linotype", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblCorreoSitio.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.lblCorreoSitio.Location = new System.Drawing.Point(16, 312);
+            this.lblCorreoSitio.Name = "lblCorreoSitio";
+            this.lblCorreoSitio.Size = new System.Drawing.Size(226, 18);
+            this.lblCorreoSitio.TabIndex = 30;
+            this.lblCorreoSitio.Text = "Ingrese el correo eléctronico del sitio:";
+            // 
+            // txtHorarioAtencion
+            // 
+            this.txtHorarioAtencion.Location = new System.Drawing.Point(12, 400);
+            this.txtHorarioAtencion.Name = "txtHorarioAtencion";
+            this.txtHorarioAtencion.PasswordChar = '*';
+            this.txtHorarioAtencion.Size = new System.Drawing.Size(279, 23);
+            this.txtHorarioAtencion.TabIndex = 33;
+            this.txtHorarioAtencion.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // lblHorarioAtencion
+            // 
+            this.lblHorarioAtencion.AutoSize = true;
+            this.lblHorarioAtencion.Font = new System.Drawing.Font("Palatino Linotype", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblHorarioAtencion.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.lblHorarioAtencion.Location = new System.Drawing.Point(15, 370);
+            this.lblHorarioAtencion.Name = "lblHorarioAtencion";
+            this.lblHorarioAtencion.Size = new System.Drawing.Size(236, 18);
+            this.lblHorarioAtencion.TabIndex = 32;
+            this.lblHorarioAtencion.Text = "Ingrese el horario de atención del sitio:";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(12, 465);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.PasswordChar = '*';
+            this.textBox4.Size = new System.Drawing.Size(279, 23);
+            this.textBox4.TabIndex = 35;
+            // 
+            // lblCostoServicio
+            // 
+            this.lblCostoServicio.AutoSize = true;
+            this.lblCostoServicio.Font = new System.Drawing.Font("Palatino Linotype", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblCostoServicio.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.lblCostoServicio.Location = new System.Drawing.Point(16, 436);
+            this.lblCostoServicio.Name = "lblCostoServicio";
+            this.lblCostoServicio.Size = new System.Drawing.Size(218, 18);
+            this.lblCostoServicio.TabIndex = 34;
+            this.lblCostoServicio.Text = "Ingrese el costo de servicio del sitio:";
+            // 
+            // FrmRegistroSitios
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(304, 541);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.lblCostoServicio);
+            this.Controls.Add(this.txtHorarioAtencion);
+            this.Controls.Add(this.lblHorarioAtencion);
+            this.Controls.Add(this.txtCorreoSitio);
+            this.Controls.Add(this.lblCorreoSitio);
+            this.Controls.Add(this.txtNumeroTelefono);
+            this.Controls.Add(this.lblNumeroTelefono);
+            this.Controls.Add(this.picbxLogofrm2);
+            this.Controls.Add(this.btnGuardar);
+            this.Controls.Add(this.txtNombreDueno);
+            this.Controls.Add(this.lblNombreDueno);
+            this.Controls.Add(this.txtUbicacionSitio);
+            this.Controls.Add(this.lblUbicacionSitio);
+            this.Controls.Add(this.txtNombreSitio);
+            this.Controls.Add(this.lblNombreSitio);
+            this.Controls.Add(this.lblRegistroSitio);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "FrmRegistroSitios";
+            this.Text = "North Pearl";
+            ((System.ComponentModel.ISupportInitialize)(this.picbxLogofrm2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private PictureBox picbxLogofrm2;
+        private Button btnGuardar;
+        private TextBox txtNombreDueno;
+        private Label lblNombreDueno;
+        private TextBox txtUbicacionSitio;
+        private Label lblUbicacionSitio;
+        private TextBox txtNombreSitio;
+        private Label lblNombreSitio;
+        private Label lblRegistroSitio;
+        private TextBox txtNumeroTelefono;
+        private Label lblNumeroTelefono;
+        private TextBox txtCorreoSitio;
+        private Label lblCorreoSitio;
+        private TextBox txtHorarioAtencion;
+        private Label lblHorarioAtencion;
+        private TextBox textBox4;
+        private Label lblCostoServicio;
+    }
+}
